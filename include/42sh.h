ccdc14cf20f4820e37b7dc7abe43927e542d2d08
izissise <hugues.m@hotmail.fr>
2013-03-29 10:56:13 +0100

    Add struct in include

--- include/42sh.h
+++ include/42sh.h
@@ -9,12 +9,47 @@
 */
 
 #ifndef		42SH_H
-#define		42SH_H
+# define		42SH_H
+
+typedef struct	s_fds
+{
+  int		stdin;
+  int		stdout;
+  int		stderr;
+}		t_fds;
+
+typedef struct	s_pid
+{
+  pid_t		sid;
+  pid_t		pid;
+  pid_t		pgid;
+}		t_pid;
+
+typedef struct	s_cmd
+{
+  char		*line;
+  char		**argv;
+  int		return_value;
+  t_fds		fd;
+  t_pid		pid;
+}		t_cmd;
+
+typedef struct	s_pipe
+{
+  t_fds		fd;
+  t_pid		pid;
+  char		*line;
+  t_cmd		**cmds;
+  int		running;
+}		t_pipe;
 
 typedef struct	s_sh
 {
+  t_pid		pid;
   char		**path;
   char		**env;
+  t_pipe		**pipes;
+  t_pipe		*forground;
 }		t_sh;
 
 #endif
