9be64fb7f7efaf123fe15c64b4a1f6a079c39c1e
maxime94 <maxime.l@live.fr>
2013-04-10 01:41:16 +0200

    cd  ok mais pas cd -

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -5,13 +5,17 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Tue Apr  9 23:41:56 2013 maxime lavandier
+** Last update Wed Apr 10 01:40:40 2013 maxime lavandier
 */
 
 #include "../../include/42sh.h"
 
 char	*builtin_cd_env(t_sh *shell, char *path, char *temp)
 {
+  getcwd(temp, 4095);
+  path = malloc(my_strlen(temp) + 5);
+  my_strncpy(path, "PWD=", -1);
+  my_strncpy(&(path[4]), temp, -1);
   if (get_envvar("PWD", shell->env) != NULL)
     rm_ptr_f_tab((void **)shell->env, get_envvar("PWD", shell->env) - 4);
   shell->env = (char **) add_ptr_t_tab((void **)shell->env, path);
@@ -25,22 +29,26 @@ void		builtin_cd(t_sh *shell, t_cmd *cmd)
   char		*old_pwd;
 
   getcwd(temp, 4095);
+  old_pwd = get_envvar("OLD_PWD", shell->env);
   if ((cmd->argv[1] != NULL) && (my_strncmp(cmd->argv[1], "-", -1) == 0)
-      && (old_pwd = get_envvar("OLD_PWD", shell->env)) != NULL)
+      && old_pwd != NULL)
     {
       chdir(old_pwd);
       rm_ptr_f_tab((void **) shell->env, (void *) old_pwd - 8);
       path = malloc(my_strlen(temp) + 9);
       my_strncpy(path, "OLD_PWD=", -1);
       my_strncpy(&(path[8]), old_pwd, -1);
+      shell->env = add_ptr_t_tab((void **)shell->env, path);
     }
   else if (cmd->argv[1] != NULL)
     {
+      if (old_pwd != NULL)
+	rm_ptr_f_tab((void **) shell->env, (void *) old_pwd - 8);
+      path = malloc(my_strlen(temp) + 9);
+      my_strncpy(path, "OLD_PWD=", -1);
+      my_strncpy(&(path[8]), temp, -1);
+      shell->env = add_ptr_t_tab((void **)shell->env, path);
       chdir(cmd->argv[1]);
-      getcwd(temp, 4095);
-      path = malloc(my_strlen(temp) + 5);
-      my_strncpy(path, "PWD=", -1);
-      my_strncpy(&(path[4]), temp, -1);
     }
   else
     return ;
