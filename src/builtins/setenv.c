7248934a85550c94fbf9eae36037cd7e4608d3d9
maxime94 <maxime.l@live.fr>
2013-04-10 02:24:23 +0200

    setenv et unsetenv ok

--- src/builtins/setenv.c
+++ src/builtins/setenv.c
@@ -5,7 +5,7 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Tue Apr  9 15:42:01 2013 maxime lavandier
+** Last update Wed Apr 10 02:23:18 2013 maxime lavandier
 */
 
 #include "../../include/42sh.h"
@@ -26,4 +26,6 @@ void	builtin_setenv(t_sh *shell, t_cmd *cmd)
   my_strncpy(&(temp[my_strlen(temp)]), "=", -1);
   my_strncpy(&(temp[my_strlen(temp)]), cmd->argv[2], -1);
   shell->env = (char **) add_ptr_t_tab((void **) shell->env, temp);
+  if (my_strncmp(cmd->argv[1], "PATH", -1) == 0)
+    shell->path = str_to_wordtab(cmd->argv[2], ":", 0);
 }
--- src/builtins/unsetenv.c
+++ src/builtins/unsetenv.c
@@ -5,15 +5,22 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Tue Apr  9 15:53:25 2013 maxime lavandier
+** Last update Wed Apr 10 02:23:48 2013 maxime lavandier
 */
 
 #include "../../include/42sh.h"
 
 void	builtin_unsetenv(t_sh *shell, t_cmd *cmd)
 {
+  char	*temp;
+
   if (cmd->argv[1] == 0)
     write(1, "unsetenv: Trop peu d'arguments.\n", 32);
-  if (get_envvar(cmd->argv[1], shell->env) != 0)
-    rm_ptr_f_tab((void **) shell->env, cmd->argv[1]);
+  if ((temp = get_envvar(cmd->argv[1], shell->env)) != NULL)
+    rm_ptr_f_tab((void **) shell->env, temp - my_strlen(cmd->argv[1]) - 1);
+  if (my_strncmp(cmd->argv[1], "PATH", -1) == 0)
+    {
+      free_ptr_tab((void **) shell->path);
+      shell->path = 0;
+    }
 }
