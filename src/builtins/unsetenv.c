f7f0abfad6babd6fa36d82374694092d8b3c10fc
izissise <hugues.m@hotmail.fr>
2013-04-10 02:35:16 +0200

    change a message in unsetenv

--- src/builtins/unsetenv.c
+++ src/builtins/unsetenv.c
@@ -14,8 +14,8 @@ void	builtin_unsetenv(t_sh *shell, t_cmd *cmd)
 {
   char	*temp;
 
-  if (cmd->argv[1] == 0)
-    write(1, "unsetenv: Trop peu d'arguments.\n", 32);
+  if (cmd->argv[1] == NULL)
+    my_putstr("Unsetenv: missing argument\n", 2, -1);
   if ((temp = get_envvar(cmd->argv[1], shell->env)) != NULL)
     rm_ptr_f_tab((void **) shell->env, temp - my_strlen(cmd->argv[1]) - 1);
   if (my_strncmp(cmd->argv[1], "PATH", -1) == 0)
