b9e0fbaa610240cd658f0ee9a9cc5acd8a009831
remi <remi@remi-HP-EliteBook-8560p.(none)>
2013-04-14 10:54:55 +0200

    warinig compilation reglé

--- src/edition_line/init_struct_param.c
+++ src/edition_line/init_struct_param.c
@@ -5,20 +5,15 @@
 ** Login   <robert_r@epitech.net>
 **
 ** Started on  Tue Apr  9 15:26:37 2013 remi robert
-** Last update Sun Apr 14 10:13:21 2013 remi robert
+** Last update Sun Apr 14 10:52:23 2013 remi robert
 */
 
 #include "../include/my_func.h"
 
 void		init_struct_param(t_param *param)
 {
-  static int	i = 0;
-
   mod_raw();
   get_pos_curser(&(param->begin_pos.x), &(param->begin_pos.y));
-  /* if (i != 0) */
-  /*   param->begin_pos.x += my_strlen(param->str_prompt); */
-  /* i = 1; */
   param->pos_history = 0;
   param->current_pos.x = param->begin_pos.x;
   param->current_pos.y = param->begin_pos.y;
