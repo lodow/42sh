34ee71d6123148ee394f6de43cfc61ca1ffa538e
izissise <hugues.m@hotmail.fr>
2013-04-15 15:56:09 +0200

    Begin echo builtins

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -10,7 +10,27 @@
 
 #include "42sh.h"
 
+int	builtins_print_echo(char *path, char **argv, t_sh *shell)
+{
+  int	i;
+  int	first;
+
+  i = 1;
+  first = 1;
+  while (argv[i] != NULL)
+    {
+      if (first == 0)
+        my_putstr(" ", 1, -1);
+      my_putstr(argv[i], 1, -1);
+      first = 0;
+      i++;
+    }
+  my_putstr("\n", 1, -1);
+  my_exit(0);
+  return (-1);
+}
+
 void	builtin_echo(t_cmd *cmd, t_fds *fd, t_sh *shell)
 {
-
+  exec_process(cmd, fd, shell, &builtins_print_echo);
 }
