ab04a5d7b564bab94122bc911d2c388a097c0c5b
maxime94 <maxime.l@live.fr>
2013-04-16 15:29:24 +0200

    builtin clear ok

--- src/builtins/clear.c
+++ src/builtins/clear.c
@@ -5,13 +5,21 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Mon Oct  8 16:20:21 2012 hugues morisset
+** Last update Tue Apr 16 15:27:34 2013 maxime lavandier
 */
 
 #include "42sh.h"
 
 void	builtin_clear(t_cmd *cmd, t_fds *fd, t_sh *shell)
 {
+  char	*str;
 
+  str  = tgetstr("cl", 0);
+  if (str == NULL)
+    {
+      write(2, "Clear error\n", 12);
+      return ;
+    }
+  write(1, str, strlen(str));
 }
 
