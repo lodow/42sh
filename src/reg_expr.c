f76f259c406a89cfd4b05159c816f98e534c4b3c
izissise <hugues.m@hotmail.fr>
2013-04-30 14:07:16 +0200

    Return a tab matching a regexp

--- src/reg_expr.c
+++ src/reg_expr.c
@@ -10,4 +10,24 @@
 
 #include "include.h"
 
+char	**expand_reg_exp(char *regexp)
+{
+  glob_t	res;
+  char	**tab;
+  int	glob_ret;
+  int	flags;
+  int	i;
 
+  flags = GLOB_MARK | GLOB_NOCHECK | GLOB_BRACE | GLOB_TILDE;
+  glob_ret = glob(regexp, flags, NULL, &res);
+  i = 0;
+  tab = NULL;
+  while (i < res.gl_pathc)
+    {
+      tab = (char**)add_ptr_t_tab((void**)tab, (void*)res.gl_pathv[i]);
+      printf(tab[i]);
+      i++;
+    }
+  globfree(&res);
+  return (tab);
+}
