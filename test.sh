f880ca7315e1a41f5f104d30385234124fb88c07
izissise <hugues.m@hotmail.fr>
2013-05-22 15:54:30 +0200

    test script #38

---
+++ test.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+SHB=/bin/bash
+SH=./42SH
+OUT=tmp
+OUTB=tmp2
+
+COMMAND=""
+
+check_diff() {
+ echo ${COMMAND} | ${SH} > ${OUT} 2>&1
+ echo ${COMMAND} | ${SHB} > ${OUTB} 2>&1
+ diff ${OUT} ${OUTB}
+}
+
+COMMAND="|"
+check_diff | less
+
+COMMAND=";"
+check_diff | less
+
+COMMAND="&"
+check_diff | less
+
+COMMAND="&&"
+check_diff | less
+
+COMMAND="||"
+check_diff | less
+
+COMMAND="\\"
+check_diff | less
+
+COMMAND="ls | | | ||||||||"
+check_diff | less
+
+COMMAND=";;;;;;;;ls;;;;;;;;;"
+check_diff | less
+
+COMMAND="\"\"\"\\\"\"\"\""
+check_diff | less
+
+COMMAND="ls | | | ||||||||"
+check_diff | less
+
+COMMAND="env env env env env env env -i env ls"
+check_diff | less
+
+COMMAND="cd /; cd -; cd /; cd ../; cd;"
+check_diff | less
+
+COMMAND="ls && dfe"
+check_diff | less
+
+COMMAND="ls ../ && ls"
+check_diff | less
+
+COMMAND="ls || ls ../"
+check_diff | less
+
+COMMAND="echo -n test"
+check_diff | less
+
+COMMAND="cd .. && ls"
+check_diff | less
+
+COMMAND="env | cat -e"
+check_diff | less
+
+rm -f ${OUT} ${OUTB}
+
+
