ffa1da990610390ff5b04c86a74d383cca1f5afd
maxime94 <maxime.l@live.fr>
2013-05-26 01:54:47 +0200

    antiboucle infini

--- src/edition_line/termcap/get_pos_curser.c
+++ src/edition_line/termcap/get_pos_curser.c
@@ -5,7 +5,7 @@
 ** Login   <remi@epitech.net>
 **
 ** Started on  Thu Mar 21 08:21:11 2013 remi
-** Last update Sat May 25 23:53:27 2013 maxime lavandier
+** Last update Sun May 26 01:52:24 2013 maxime lavandier
 */
 
 #include "42sh.h"
@@ -41,16 +41,17 @@ void	get_pos_curser(int *x, int *y, int fd)
 {
   char	buff[30];
   int	indice;
+  int	i;
 
   *x = 0;
   *y = 0;
+  i = 0;
   my_memset(buff, 30, 0);
-  if (write(fd, POSCURSEUR, my_strlen(POSCURSEUR)) == -1)
-    return ;
-  if ((indice = read(fd, buff, 19)) == -1)
+  if (write(fd, POSCURSEUR, my_strlen(POSCURSEUR)) == -1 ||
+      (indice = read(fd, buff, 19)) == -1)
     return ;
   buff[indice] = '\0';
-  while (buff[0] != 27)
+  while (buff[0] != 27 && i < 100)
     {
       my_memset(buff, 30, 0);
       if (write(fd, POSCURSEUR, my_strlen(POSCURSEUR)) == -1)
@@ -58,6 +59,9 @@ void	get_pos_curser(int *x, int *y, int fd)
       if ((indice = read(fd, buff, 19)) == -1)
 	return ;
       buff[indice] = '\0';
+      i++;
     }
+  if (i == 100)
+    return ;
   assign_value_pos(x, y, buff);
 }
