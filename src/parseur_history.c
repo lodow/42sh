4beba83e66b7a928125ec0d4eb95c92edd93d32c
izissise <hugues.m@hotmail.fr>
2013-05-26 17:02:25 +0200

    Remove useless func

--- src/parseur_history.c
+++ src/parseur_history.c
@@ -10,51 +10,6 @@
 
 #include "42sh.h"
 
-int	extract_cmd_history(char *cmd, t_history *history, char *str)
-{
-  int	indice;
-  int	indice_nb;
-
-  indice = 0;
-  while (cmd != NULL && history != NULL &&
-	 cmd[indice] != '\0')
-    {
-      if (cmd[indice] == '!')
-	{
-	  indice_nb = 1;
-	  str[0] = '!';
-	  while (cmd[indice] != '\0' && cmd[indice] >= '0' &&
-		 cmd[indice] <= '9' && indice_nb < 12)
-	    str[indice_nb++] = cmd[indice++];
-	  str[indice_nb] = '\0';
-	  if (str[1] != '\0')
-	    return (1);
-	}
-      if (cmd[indice])
-	++indice;
-    }
-  return (0);
-}
-
-char	*cmd_copy_hist(char *cmd)
-{
-  char	*s;
-  int	indice;
-
-  if (cmd == NULL || (s = malloc(my_strlen(cmd) + 2)) == NULL)
-    return (NULL);
-  indice = 0;
-  while (cmd[indice] != '\0')
-    {
-      s[indice] = cmd[indice];
-      ++indice;
-    }
-  s[indice] = ' ';
-  s[indice + 1] = '\0';
-  free(cmd);
-  return (s);
-}
-
 void	parseur_history(char **cmd, t_history *history)
 {
   char	*inter;
